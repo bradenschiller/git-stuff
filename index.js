cc8684b94ada04b5b0893234fea12beb2ffa781d
bradenschiller <bradenschiller17@gmail.com>
2021-05-21 10:15:19 -0500

    second commit: made some changes to index file

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log('You made a commit')
+
+console.log('hello world')
+
+// making a change here
+
+const addFn = (num1, num2) => {
+   return num1 + num2 
+}
+
+console.log(addFn(3, 8))
+
+
+const helloWorld = (args) => {
+  if (!args) {
+     return null
+  } 
+
+  return args
+}
+
+console.log(helloWorld("Hello World"))
